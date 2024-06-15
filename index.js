1b58867e9f802f4da9d308d28bab9d032583fe71
thekamalkashyap <iamkamalkumar@proton.me>
2024-06-16 00:57:36 +0530

    fixed

--- index.js
+++ index.js
@@ -3,43 +3,44 @@ const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
 function showNotes() {
-    notesContainer.innerHTML = localStorage.getItem("notes");
+  notesContainer.innerHTML = localStorage.getItem("notes");
 }
 
 showNotes();
 
 function updateStorage() {
-    localStorage.setItem("notes", notesContainer.innerHTML);
+  localStorage.setItem("notes", notesContainer.innerHTML);
 }
 
-createBtn.addEventListener("click", ()=>{
-    let inputBox = document.createElement("p");
-    let img = document.createElement("img");
-    inputBox.className = "input-box";
-    inputBox.setAttribute("contenteditable", "true");
-    img.src = "images/delete.png";
-    notesContainer.appendChild(inputBox).appendChild(img);
-})
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  inputBox.addEventListener("keypress", (e) => {
+    updateStorage();
+  });
+  img.src = "images/delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+});
 
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "p") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
 
-notesContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "IMG") {
-        e.target.parentElement.remove();
-        updateStorage(); 
-    }
-    else if(e.target.tagName === "p"){
-        notes = document.querySelectorAll(".input-box");
-        notes.forEach(nt => {
-            nt.onkeyup = function() {
-                updateStorage();
-            }
-        })
-    }
-})
-
-document.addEventListener("keydown", event =>{
-    if(event.key === "Enter"){
-        document.execCommand("insertLinerBreak");
-        event.preventDefault();
-    }
-})
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLinerBreak");
+    event.preventDefault();
+  }
+});
